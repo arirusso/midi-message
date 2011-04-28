caedcc374978aa02bca554b36ce5f5ac39b8ec9b
Ari Russo <ari.russo@gmail.com>
2011-04-28 12:32:54 -0400

    channel/velocity

--- examples/melody.rb
+++ examples/melody.rb
@@ -9,13 +9,16 @@ $LOAD_PATH.unshift dir + '/../lib'
 require 'midi-message'
 require 'pp'
 
+channel = 0
 notes = [36, 40, 43] # C E G
 octaves = 5
+velocity = 100
+
 melody = []
 
 (0..((octaves-1)*12)).step(12) do |oct|
 
-  notes.each { |note| melody << MIDIMessage::NoteOn.new(0, note + oct, 100) }
+  notes.each { |note| melody << MIDIMessage::NoteOn.new(channel, note + oct, velocity) }
     
 end
 
