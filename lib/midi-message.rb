626c27c96d92a2b181e1912d12a09a1e4d52dc1c
Ari Russo <ari.russo@gmail.com>
2011-04-28 11:44:49 -0400

    no io

---
+++ lib/midi-message.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+#
+# MIDI Messages in Ruby
+#
+module MIDIMessage
+  
+  VERSION = "0.0.1"
+ 
+end
+
+require 'midi-message/channel_message'
+require 'midi-message/node'
+require 'midi-message/system_message'
+require 'midi-message/system_exclusive'
