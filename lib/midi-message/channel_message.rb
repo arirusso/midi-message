1b1144fb9be1d340e12bc758bf7dba4c631f96a6
Ari Russo <ari.russo@gmail.com>
2011-04-30 14:55:19 -0400

    parse bug, sysex example/refactoring

--- lib/midi-message/channel_message.rb
+++ lib/midi-message/channel_message.rb
@@ -1,208 +1,206 @@
 #!/usr/bin/env ruby
+#
 # (c)2011 Ari Russo and licensed under the Apache 2.0 License
-# 
+#
 module MIDIMessage
 
-    # common behavior amongst Channel Message types
-    module ChannelMessageBehavior
+  # common behavior amongst Channel Message types
+  module ChannelMessageBehavior
 
-      attr_reader :data,
+    attr_reader :data,
                   :name,
                   :status
-                  
-      def initialize_channel_message(status_nibble_1, status_nibble_2, data_byte_1, data_byte_2 = 0)
-        @status = [status_nibble_1, status_nibble_2]
-        @data = [data_byte_1]
-        @data[1] = data_byte_2 if self.class::second_data_byte?
-        initialize_shortcuts
-        initialize_simple_message(status_nibble_1, status_nibble_2)
+    def initialize_channel_message(status_nibble_1, status_nibble_2, data_byte_1, data_byte_2 = 0)
+      @status = [status_nibble_1, status_nibble_2]
+      @data = [data_byte_1]
+      @data[1] = data_byte_2 if self.class::second_data_byte?
+      initialize_shortcuts
+      initialize_simple_message(status_nibble_1, status_nibble_2)
+    end
+
+    def to_a
+      db2 = self.class::second_data_byte? ? @data[1] : nil
+      [@status[0] + @status[1], @data[0], db2].compact
+    end
+    alias_method :to_byte_array, :to_a
+    alias_method :to_bytes, :to_a
+
+    def to_hex_s
+      to_a.join
+    end
+    alias_method :hex, :to_hex_s
+
+    def initialize(*a)
+      options = a.pop if a.last.kind_of?(Hash)
+      unless options.nil? || options[:const].nil?
+        key = self.class::ConstProp rescue nil
+        ind = self.class::Shortcuts.index(key) rescue nil
+      ind ||= 0
+      a.insert(ind, options[:const])
+      end
+      initialize_channel_message(self.class::TypeId, *a)
+    end
+
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def type_for_status
+        0 + (const_get(:TypeId) << 4)
       end
 
-      def to_a
-        db2 = self.class::second_data_byte? ? @data[1] : nil
-        [@status[0] + @status[1], @data[0], db2].compact
+      def type_id(id)
+        const_set(:TypeId, id)
       end
-      alias_method :to_byte_array, :to_a
-      alias_method :to_bytes, :to_a
 
-      def to_hex_s
-        to_a.join
+      def schema(*args)
+        self.send(:const_set, :Shortcuts, args)
+        const_set(:NumDataBytes, args.length-1)
       end
-      alias_method :hex, :to_hex_s
+      alias_method :layout, :schema
 
-      def initialize(*a)
-	options = a.pop if a.last.kind_of?(Hash)
-	unless options[:const].nil?
-	  key = self.class::ConstProp rescue nil
-	  ind = self.class::Shortcuts.index(key) rescue nil
-	  ind ||= 0
-	  a.insert(ind, options[:const])
+      def second_data_byte?
+        num = const_get(:NumDataBytes)
+        !num.nil? && num > 1
+      end
+
+    end
+
+    def initialize_shortcuts
+      props = [
+        { :name => :status, :index => 1 },
+        { :name => :data, :index => 0 },
+        { :name => :data, :index => 1 }
+      ]
+      shortcuts = self.class.send(:const_get, :Shortcuts)
+      shortcuts.each_with_index do |prop,i|
+        self.class.send(:attr_reader, prop)
+        self.class.send(:define_method, "#{prop}=") do |val|
+          send(:instance_variable_set, "@#{prop.to_s}", val)
+          send(props[i][:name])[props[i][:index]] = val
         end
-        initialize_channel_message(self.class::TypeId, *a)
+        instance_variable_set("@#{prop}", send(props[i][:name])[props[i][:index]])
       end
-
-      def self.included(base)
-        base.extend(ClassMethods)
-      end
-
-      module ClassMethods
-
-        def type_for_status
-          0 + (const_get(:TypeId) << 4)
-        end
-
-        def type_id(id)
-          const_set(:TypeId, id)
-        end
-
-        def schema(*args)
-          self.send(:const_set, :Shortcuts, args)
-          const_set(:NumDataBytes, args.length-1)
-        end
-        alias_method :layout, :schema
-
-        def second_data_byte?
-          num = const_get(:NumDataBytes)
-          !num.nil? && num > 1
-        end
-
-      end
-
-      def initialize_shortcuts
-         props = [
-           { :name => :status, :index => 1 }, 
-           { :name => :data, :index => 0 },
-           { :name => :data, :index => 1 }
-         ]
-	 shortcuts = self.class.send(:const_get, :Shortcuts)
-	 shortcuts.each_with_index do |prop,i|
-           self.class.send(:attr_reader, prop)
-	   self.class.send(:define_method, "#{prop}=") do |val|
-	     send(:instance_variable_set, "@#{prop.to_s}", val)
-             send(props[i][:name])[props[i][:index]] = val
-	   end
-           instance_variable_set("@#{prop}", send(props[i][:name])[props[i][:index]])
-         end
-      end
-
     end
 
-    # use this if you want to instantiate a raw channel message
-    #
-    # example = ChannelMessage.new(0x9, 0x0, 0x40, 0x57) # creates a raw note-on message
-    #
-    class ChannelMessage
+  end
 
-      include SimpleMessageBehavior
-      include ChannelMessageBehavior
+  # use this if you want to instantiate a raw channel message
+  #
+  # example = ChannelMessage.new(0x9, 0x0, 0x40, 0x57) # creates a raw note-on message
+  #
+  class ChannelMessage
 
-      display_name 'Channel Message'
-      
-      def initialize(*a)
-        initialize_channel_message(*a)
-      end
+    include SimpleMessageBehavior
+    include ChannelMessageBehavior
 
+    display_name 'Channel Message'
+    def initialize(*a)
+      initialize_channel_message(*a)
     end
 
-    #
-    # MIDI Channel Aftertouch message
-    #
-    class ChannelAftertouch
+  end
 
-      include SimpleMessageBehavior
-      include ChannelMessageBehavior
+  #
+  # MIDI Channel Aftertouch message
+  #
+  class ChannelAftertouch
 
-      schema :channel, :value
-      type_id 0xD
-      display_name 'Channel Aftertouch'
+    include SimpleMessageBehavior
+    include ChannelMessageBehavior
 
-    end
+    schema :channel, :value
+    type_id 0xD
+    display_name 'Channel Aftertouch'
 
-    #
-    # MIDI Control Change message
-    #
-    class ControlChange
+  end
 
-      include SimpleMessageBehavior
-      include ChannelMessageBehavior
+  #
+  # MIDI Control Change message
+  #
+  class ControlChange
 
-      schema :channel, :number, :value
-      type_id 0xB
-      display_name 'Control Change'
-      use_constants 'Control Change', :for => 'Number'
+    include SimpleMessageBehavior
+    include ChannelMessageBehavior
 
-    end
+    schema :channel, :number, :value
+    type_id 0xB
+    display_name 'Control Change'
+    use_constants 'Control Change', :for => 'Number'
 
-    #
-    # MIDI Note-Off message
-    #
-    class NoteOff
+  end
 
-      include SimpleMessageBehavior
-      include ChannelMessageBehavior
+  #
+  # MIDI Note-Off message
+  #
+  class NoteOff
 
-      schema :channel, :note, :velocity
-      type_id 0x8
-      display_name 'Note Off'
-      use_constants 'Note', :for => :note
+    include SimpleMessageBehavior
+    include ChannelMessageBehavior
 
-    end
+    schema :channel, :note, :velocity
+    type_id 0x8
+    display_name 'Note Off'
+    use_constants 'Note', :for => :note
 
-    #
-    # MIDI Note-On message
-    #
-    class NoteOn
+  end
 
-      include SimpleMessageBehavior
-      include ChannelMessageBehavior
+  #
+  # MIDI Note-On message
+  #
+  class NoteOn
 
-      schema :channel, :note, :velocity
-      type_id 0x9
-      display_name 'Note On'
-      use_constants 'Note', :for => :note
+    include SimpleMessageBehavior
+    include ChannelMessageBehavior
 
-    end
+    schema :channel, :note, :velocity
+    type_id 0x9
+    display_name 'Note On'
+    use_constants 'Note', :for => :note
 
-    #
-    # MIDI Pitch Bend message
-    #
-    class PitchBend
+  end
 
-      include SimpleMessageBehavior
-      include ChannelMessageBehavior
+  #
+  # MIDI Pitch Bend message
+  #
+  class PitchBend
 
-      schema :channel, :low, :high
-      type_id 0xE
-      display_name 'Pitch Bend'
+    include SimpleMessageBehavior
+    include ChannelMessageBehavior
 
-    end
+    schema :channel, :low, :high
+    type_id 0xE
+    display_name 'Pitch Bend'
 
-    #
-    # MIDI Polyphonic (note specific) Aftertouch message
-    #
-    class PolyphonicAftertouch
+  end
 
-      include SimpleMessageBehavior
-      include ChannelMessageBehavior
+  #
+  # MIDI Polyphonic (note specific) Aftertouch message
+  #
+  class PolyphonicAftertouch
 
-      schema :channel, :note, :value
-      type_id 0xA
-      display_name 'Polyphonic Aftertouch'
+    include SimpleMessageBehavior
+    include ChannelMessageBehavior
 
-    end
+    schema :channel, :note, :value
+    type_id 0xA
+    display_name 'Polyphonic Aftertouch'
 
-    #
-    # MIDI Program Change message
-    #
-    class ProgramChange
+  end
 
-      include SimpleMessageBehavior
-      include ChannelMessageBehavior
+  #
+  # MIDI Program Change message
+  #
+  class ProgramChange
 
-      schema :channel, :program
-      type_id 0xC
-      display_name 'Program Change'
+    include SimpleMessageBehavior
+    include ChannelMessageBehavior
 
-    end
-    
+    schema :channel, :program
+    type_id 0xC
+    display_name 'Program Change'
+
+  end
+
 end
--- lib/midi-message/system_exclusive.rb
+++ lib/midi-message/system_exclusive.rb
@@ -1,70 +1,42 @@
 #!/usr/bin/env ruby
 #
 # (c)2011 Ari Russo and licensed under the Apache 2.0 License
-# 
+#
 
 module MIDIMessage
 
   # MIDI System-Exclusive Messages (SysEx)
   module SystemExclusive
 
-    # convert raw MIDI data to SysEx message objects
-    def self.new(*bytes)
-      
-      start_status = bytes.shift
-      end_status = bytes.pop
-      
-      return nil unless start_status.eql?(0xF0) && end_status.eql?(0xF7)
-      
-      fixed_length_message_part = bytes.slice!(0,7)
-
-      manufacturer_id = fixed_length_message_part[0]
-      device_id = fixed_length_message_part[1]
-      model_id = fixed_length_message_part[2]
-      
-      msg_class = case fixed_length_message_part[3]
-        when 0x11 then Request
-        when 0x12 then Command
-      end
-
-      address = fixed_length_message_part.slice(4,3)
-      checksum = bytes.slice!((bytes.length - 1), 1)
-      value = bytes
-    
-      node = Node.new(manufacturer_id, model_id, :device_id => device_id)
-      msg_class.new(address, value, :checksum => checksum, :node => node)
-    end
-  
     # basic SysEx data that a message class will contain
     module Data
-    
+
       attr_reader :address,
                   :checksum,
                   :device
 
       StartByte = 0xF0
       EndByte = 0xF7
-    
       # an array of message parts.  multiple byte parts will be represented as an array of bytes
       def to_a
         array
       end
-    
+
       # a flat array of message bytes
       def to_byte_array
         array.flatten
       end
-    
+
       private
-    
+
       def address_to_i
         address.inject { |a,b| a + b }
       end
-    
+
       def value_to_i
         value.kind_of?(Array) ? value.inject { |a,b| a + b } : value
       end
-    
+
       def initialize_sysex(address, options = {})
         @node = options[:node]
         @checksum = options[:checksum]
@@ -87,33 +59,32 @@ module MIDIMessage
           EndByte
         ]
       end
-    
+
     end
-  
+
     # A SysEx command message
     #
     class Command
-    
+
       include Data
-    
+
       attr_reader :data
 
       StatusByte = 0x12
-    
       def initialize(address, data, options = {})
         @data = data
         initialize_sysex(address, options)
       end
-    
+
       def value
         @data
       end
-    
+
       def data=(val)
         @data = val
         update_byte_array
       end
-    
+
     end
 
     #
@@ -121,41 +92,66 @@ module MIDIMessage
     # is being sent to or received from.
     #
     class Node
-    
+
       attr_accessor :device_id # (Not to be confused with any kind of Device class in this library)
       attr_reader :manufacturer, :model_id
-    
       def initialize(manufacturer, model_id, options = {})
         @device_id = options[:device_id]
         @model_id = model_id
         @manufacturer = manufacturer
       end
-    
+
       def message(*a)
         a << { :node => self }
         Command.new(*a)
       end
-    
+
     end
-  
+
     # A SysEx request message
     #
     class Request
-    
+
       include Data
-    
+
       attr_accessor :size
       alias_method :value, :size
 
       StatusByte = 0x11
-    
       def initialize(address, size, options = {})
         @size = size
         initialize_sysex(address, options = {})
       end
-    
+
     end
-  
+
+    # convert raw MIDI data to SysEx message objects
+    def self.new(*bytes)
+
+      start_status = bytes.shift
+      end_status = bytes.pop
+
+      return nil unless start_status.eql?(0xF0) && end_status.eql?(0xF7)
+
+      fixed_length_message_part = bytes.slice!(0,7)
+
+      manufacturer_id = fixed_length_message_part[0]
+      device_id = fixed_length_message_part[1]
+      model_id = fixed_length_message_part[2]
+
+      msg_class = case fixed_length_message_part[3]
+      when 0x11 then Request
+      when 0x12 then Command
+      end
+
+      address = fixed_length_message_part.slice(4,3)
+      checksum = bytes.slice!((bytes.length - 1), 1)
+      value = bytes
+
+      node = Node.new(manufacturer_id, model_id, :device_id => device_id)
+      msg_class.new(address, value, :checksum => checksum, :node => node)
+    end
+
   end
 
 end
