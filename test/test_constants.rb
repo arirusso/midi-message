996e05fdf87688b3c655d3e2150535f1173846a2
Ari Russo <ari.russo@gmail.com>
2011-05-01 14:01:15 -0400

    constants test

---
+++ test/test_constants.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + '/test_helper'
+
+class ConstantsTest < Test::Unit::TestCase
+
+  include MIDIMessage
+  include TestHelper        
+   
+end
