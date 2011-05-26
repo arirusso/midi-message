df88cdacf1b9f7ea0cf6d930f2753e5b78a00f01
Ari Russo <ari.russo@gmail.com>
2011-05-25 20:45:20 -0400

    more tests for constants

--- test/test_constants.rb
+++ test/test_constants.rb
@@ -14,6 +14,23 @@ class ConstantsTest < Test::Unit::TestCase
     assert_equal([0x90, 0x30, 100], msg.to_a)  
   end 
   
+  def test_control_change
+    msg = NoteOn["C3"].new(0, 100)
+    assert_equal(NoteOn, msg.class)
+    assert_equal("C3", msg.name)
+    assert_equal([0x90, 0x30, 100], msg.to_a)      
+  end
+  
+  def test_control_change
+    msg = ControlChange["Modulation Wheel"].new(2, 0x20)
+    assert_equal(ControlChange, msg.class)
+    assert_equal(msg.channel, 2)
+    assert_equal(0x01, msg.index)
+    assert_equal(0x20, msg.value)
+    assert_equal([0xB2, 0x01, 0x20], msg.to_a)    
+  end
+
+  
   def test_system_realtime
     msg = SystemRealtime["Stop"].new
     assert_equal(SystemRealtime, msg.class)
@@ -22,7 +39,10 @@ class ConstantsTest < Test::Unit::TestCase
   end  
   
   def test_system_common
-     
+    msg = SystemCommon["Song Select"].new
+    assert_equal(SystemCommon, msg.class)
+    assert_equal("Song Select", msg.name)
+    assert_equal([0xF3], msg.to_a)      
   end  
   
 end
