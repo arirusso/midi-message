b6063fe24cd1f8703eed6626806d3d09529ed646
Ari Russo <ari.russo@gmail.com>
2011-05-26 22:49:20 -0400

    test case-insensitive constant

--- test/test_constants.rb
+++ test/test_constants.rb
@@ -8,7 +8,7 @@ class ConstantsTest < Test::Unit::TestCase
   include TestHelper        
    
   def test_note_on
-    msg = NoteOn["C3"].new(0, 100)
+    msg = NoteOn["c3"].new(0, 100)
     assert_equal(NoteOn, msg.class)
     assert_equal("C3", msg.name)
     assert_equal([0x90, 0x30, 100], msg.to_a)  
