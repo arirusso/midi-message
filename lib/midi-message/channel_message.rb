1afd04d18fc05fcf56d15a2b63ce797589c53cc4
Ari Russo <ari.russo@gmail.com>
2011-08-19 19:33:29 -0400

    cleanup, comments

--- lib/midi-message/channel_message.rb
+++ lib/midi-message/channel_message.rb
@@ -134,67 +134,6 @@ module MIDIMessage
   Controller = ControlChange #shortcut
 
   #
-  # Shared Note Message Behavior
-  #
-  module NoteMessage
-
-    def octave
-      (note / 12) -1
-    end
-    alias_method :oct, :octave
-    
-    def octave=(val)
-      self.note = ((val + 1) * 12) + abs_note
-      self
-    end
-    alias_method :oct=, :octave=
-    
-    def abs_note
-      note - ((note / 12) * 12)
-    end
-    
-    def note_name
-      name.gsub(/[1-9]*/, "")
-    end
-    
-  end
-
-  #
-  # MIDI Note-Off message
-  #
-  class NoteOff
-
-    include NoteMessage
-    include ShortMessage
-    include ChannelMessage
-
-    schema :channel, :note, :velocity
-    use_display_name 'Note Off'
-    use_constants 'Note', :for => :note
-
-  end
-
-  #
-  # MIDI Note-On message
-  #
-  class NoteOn
-    
-    include NoteMessage
-    include ShortMessage
-    include ChannelMessage    
-
-    schema :channel, :note, :velocity
-    use_display_name 'Note On'
-    use_constants 'Note', :for => :note
-    
-    # returns the NoteOff equivalent of this object
-    def to_note_off
-      NoteOff.new(channel, note, velocity)
-    end
-
-  end
-
-  #
   # MIDI Pitch Bend message
   #
   class PitchBend
---
+++ lib/midi-message/note_message.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+#
+module MIDIMessage
+
+  #
+  # Common Note Message Behavior
+  #
+  module NoteMessage
+
+    # the octave number of the note
+    def octave
+      (note / 12) -1
+    end
+    alias_method :oct, :octave
+    
+    # set the octave number of the note
+    def octave=(val)
+      self.note = ((val + 1) * 12) + abs_note
+      self
+    end
+    alias_method :oct=, :octave=
+    
+    # how many half-steps is this note above the closest C
+    def abs_note
+      note - ((note / 12) * 12)
+    end
+    
+    # the name of the note without its octave e.g. F#
+    def note_name
+      name.gsub(/[1-9]*/, "")
+    end
+        
+  end
+
+  #
+  # MIDI Note-Off message
+  #
+  class NoteOff
+
+    include NoteMessage
+    include ShortMessage
+    include ChannelMessage
+
+    schema :channel, :note, :velocity
+    use_display_name 'Note Off'
+    use_constants 'Note', :for => :note
+
+  end
+
+  #
+  # MIDI Note-On message
+  #
+  class NoteOn
+    
+    include NoteMessage
+    include ShortMessage
+    include ChannelMessage    
+
+    schema :channel, :note, :velocity
+    use_display_name 'Note On'
+    use_constants 'Note', :for => :note
+    
+    # returns the NoteOff equivalent of this object
+    def to_note_off
+      NoteOff.new(channel, note, velocity)
+    end
+
+  end
+  
+end
