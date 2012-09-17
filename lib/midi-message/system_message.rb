43e20d3861422ae308aa998e36803930c769c7f5
Ari Russo <ari.russo@gmail.com>
2012-09-16 21:37:27 -0400

    bug fix for system common constants; allow redundant 0xF first nibble

--- lib/midi-message/system_message.rb
+++ lib/midi-message/system_message.rb
@@ -2,6 +2,22 @@
 #
 
 module MIDIMessage
+
+  #
+  # MIDI System message
+  #
+  module SystemMessage
+
+    # In the case of something like SystemCommon.new(0xF2, 0x00, 0x08), the first nibble F is redundant because
+    # all system messages start with 0xF and it can be assumed.
+    # However, the this method looks to see if this has occurred and strips the redundancy
+    # @param [Integer] byte The byte to strip of a redundant 0xF
+    # @return [Integer] The remaining nibble
+    def strip_redundant_nibble(byte)
+      byte > 0xF ? (byte & 0x0F) : byte
+    end
+
+  end
   
   #
   # MIDI System-Common message
@@ -9,6 +25,7 @@ module MIDIMessage
   class SystemCommon
 
     include ShortMessage
+    include SystemMessage
     use_display_name 'System Common'
     
     attr_reader :data
@@ -16,9 +33,10 @@ module MIDIMessage
     def initialize(*a)
       options = a.last.kind_of?(Hash) ? a.pop : {}
       @const = options[:const]
-      @data = [a[1], a[2]]
-      second_nibble = @const.nil? ? a[0] : @const.value 
-      initialize_short_message(0xF, second_nibble)
+      id = @const.nil? ? a.shift : @const.value
+      id = strip_redundant_nibble(id)
+      initialize_short_message(0xF, id)
+      @data = [a[0], a[1]]
     end
     
   end  
@@ -29,12 +47,14 @@ module MIDIMessage
   class SystemRealtime
 
     include ShortMessage
+    include SystemMessage
     use_display_name 'System Realtime'
     
     def initialize(*a)
       options = a.last.kind_of?(Hash) ? a.pop : {} 
       @const = options[:const]
       id = @const.nil? ? a[0] : @const.value
+      id = strip_redundant_nibble(id)
       initialize_short_message(0xF, id)
     end
 
