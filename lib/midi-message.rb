00ed3c44b6dbec9db92962803b0a889aeb9d680f
Ari Russo <ari.russo@gmail.com>
2012-09-17 00:02:30 -0400

    date

--- lib/midi-message.rb
+++ lib/midi-message.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #
 # MIDI Messages in Ruby
-# (c)2011 Ari Russo and licensed under the Apache 2.0 License
+# (c)2011-2012 Ari Russo and licensed under the Apache 2.0 License
 # 
 module MIDIMessage
   
