203169b0225dfc0e3b509c7207763045a3900184
Ari Russo <ari.russo@gmail.com>
2015-05-02 12:46:41 -0400

    use shoulda

--- test/constants_test.rb
+++ test/constants_test.rb
@@ -2,44 +2,83 @@ require "helper"
 
 class ConstantsTest < Minitest::Test
 
-  def test_note_on
-    msg = MIDIMessage::NoteOn["c3"].new(0, 100)
-    assert_equal(MIDIMessage::NoteOn, msg.class)
-    assert_equal("C3", msg.name)
-    assert_equal("C", msg.note_name)
-    assert_equal("Note On: C3", msg.verbose_name)
-    assert_equal([0x90, 0x30, 100], msg.to_a)
-  end
+  context "Constants" do
 
-  def test_note_off
-    msg = MIDIMessage::NoteOff["C2"].new(0, 100)
-    assert_equal(MIDIMessage::NoteOff, msg.class)
-    assert_equal("C2", msg.name)
-    assert_equal("Note Off: C2", msg.verbose_name)
-    assert_equal([0x80, 0x24, 100], msg.to_a)
-  end
+    context "note on" do
 
-  def test_control_change
-    msg = MIDIMessage::ControlChange["Modulation Wheel"].new(2, 0x20)
-    assert_equal(MIDIMessage::ControlChange, msg.class)
-    assert_equal(msg.channel, 2)
-    assert_equal(0x01, msg.index)
-    assert_equal(0x20, msg.value)
-    assert_equal([0xB2, 0x01, 0x20], msg.to_a)
-  end
+      setup do
+        @message = MIDIMessage::NoteOn["c3"].new(0, 100)
+      end
 
-  def test_system_realtime
-    msg = MIDIMessage::SystemRealtime["Stop"].new
-    assert_equal(MIDIMessage::SystemRealtime, msg.class)
-    assert_equal("Stop", msg.name)
-    assert_equal([0xFC], msg.to_a)
-  end
+      should "create message object" do
+        assert_equal(MIDIMessage::NoteOn, @message.class)
+        assert_equal("C3", @message.name)
+        assert_equal("C", @message.note_name)
+        assert_equal("Note On: C3", @message.verbose_name)
+        assert_equal([0x90, 0x30, 100], @message.to_a)
+      end
 
-  def test_system_common
-    msg = MIDIMessage::SystemCommon["Song Select"].new
-    assert_equal(MIDIMessage::SystemCommon, msg.class)
-    assert_equal("Song Select", msg.name)
-    assert_equal([0xF3], msg.to_a)
+    end
+
+    context "note off" do
+
+      setup do
+        @message = MIDIMessage::NoteOff["C2"].new(0, 100)
+      end
+
+      should "create message object" do
+        assert_equal(MIDIMessage::NoteOff, @message.class)
+        assert_equal("C2", @message.name)
+        assert_equal("Note Off: C2", @message.verbose_name)
+        assert_equal([0x80, 0x24, 100], @message.to_a)
+      end
+
+    end
+
+    context "cc" do
+
+      setup do
+        @message = MIDIMessage::ControlChange["Modulation Wheel"].new(2, 0x20)
+      end
+
+      should "create message object" do
+        assert_equal(MIDIMessage::ControlChange, @message.class)
+        assert_equal(@message.channel, 2)
+        assert_equal(0x01, @message.index)
+        assert_equal(0x20, @message.value)
+        assert_equal([0xB2, 0x01, 0x20], @message.to_a)
+      end
+
+    end
+
+    context "system realtime" do
+
+      setup do
+        @message = MIDIMessage::SystemRealtime["Stop"].new
+      end
+
+      should "create message object" do
+        assert_equal(MIDIMessage::SystemRealtime, @message.class)
+        assert_equal("Stop", @message.name)
+        assert_equal([0xFC], @message.to_a)
+      end
+
+    end
+
+    context "system common" do
+
+      setup do
+        @message = MIDIMessage::SystemCommon["Song Select"].new
+      end
+
+      should "create message object" do
+        assert_equal(MIDIMessage::SystemCommon, @message.class)
+        assert_equal("Song Select", @message.name)
+        assert_equal([0xF3], @message.to_a)
+      end
+
+    end
+
   end
 
 end
