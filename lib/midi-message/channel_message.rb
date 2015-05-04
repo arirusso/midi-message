c070e9b388ca7c877700eebda5ad41ce7be4382f
Ari Russo <ari.russo@gmail.com>
2015-05-03 21:37:27 -0400

    extract methods

--- lib/midi-message/channel_message.rb
+++ lib/midi-message/channel_message.rb
@@ -20,8 +20,15 @@ module MIDIMessage
     def initialize(*data)
       data = data.dup
       options = data.last.kind_of?(Hash) ? data.pop : {}
-      assign_constant_value(options[:const], data) unless options[:const].nil?
+      add_constant_value(options[:const], data) unless options[:const].nil?
       initialize_channel_message(self.class.type_for_status, *data)
+    end
+
+    private
+
+    def self.included(base)
+      base.send(:include, ::MIDIMessage::Message)
+      base.send(:extend, ClassMethods)
     end
 
     # Decorates the object with the particular properties for its type
@@ -32,22 +39,22 @@ module MIDIMessage
       end
     end
 
-    private
-
-    def self.included(base)
-      base.send(:include, ::MIDIMessage::Message)
-      base.send(:extend, ClassMethods)
-    end
-
-    def assign_constant_value(constant, data)
+    # Add the given constant to message data
+    def add_constant_value(constant, data)
       index = Constant::Loader.get_index(self)
       data.insert(index, constant.value)
     end
 
-    def initialize_channel_message(status_nibble_1, status_nibble_2, data_byte_1, data_byte_2 = 0)
+    # Assign the message data
+    def assign_data(status_nibble_1, status_nibble_2, data_byte_1, data_byte_2 = 0)
       @status = [status_nibble_1, status_nibble_2]
       @data = [data_byte_1]
       @data[1] = data_byte_2 if self.class.second_data_byte?
+    end
+
+    # Initialize the message: assign data, decorate with accessors
+    def initialize_channel_message(status_nibble_1, status_nibble_2, data_byte_1, data_byte_2 = 0)
+      assign_data(status_nibble_1, status_nibble_2, data_byte_1, data_byte_2)
       add_accessors
       initialize_message(status_nibble_1, status_nibble_2)
     end
